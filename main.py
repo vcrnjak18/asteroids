ceeb9fa16e7f814e59f9d3b0fc4b8bf989bcf673
vcrnjak18 <viktor.crnjak18@gmail.com>
2024-12-29 18:27:05 +0100

    Created parent CircleShape and child Player classes; added drawing to the screen

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	dt = 0
+
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+
 		screen.fill("#000000")
+		player.draw(screen)
 		pygame.display.flip()
 
-	print("Starting asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
+		dt = clock.tick(60)/1000
 
 
 
